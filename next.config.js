dbcc254e96c23777d5016dd3390326be4de33c87
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-04 21:04:23 +0200

    build: ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["page.js", "page.jsx", "page.ts", "page.tsx"],
+  pageExtensions: ['page.js', 'page.jsx', 'page.ts', 'page.tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
