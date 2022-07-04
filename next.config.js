9885aa52c4bc9f47241d8ae34cdbd34b669bc2c6
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-04 20:28:18 +0200

    build: change pageExtensions in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.js", "page.jsx", "page.ts", "page.tsx"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
