d90585355a25c8154412e339be0643a5b5762770
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-04 23:50:32 +0200

    fix: adjust aggregation in getListings

--- pages/api/getListings.page.ts
+++ pages/api/getListings.page.ts
@@ -2,6 +2,21 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { LISTINGS_COLLECTION_NAME } from 'services/mongodb/mongodb.constants';
 import { mongoClient } from 'services/mongodb/mongodb.service';
 
+const pipeline = [
+  { $limit: 10 },
+  {
+    $project: {
+      _id: 1,
+      name: 1,
+      summary: 1,
+      price: {
+        $toDouble: '$price',
+      },
+      images: 1,
+    },
+  },
+];
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Listing[]>
@@ -9,8 +24,9 @@ export default async function handler(
   try {
     await mongoClient.connect();
     const db = mongoClient.db();
-    const collection = db.collection<Listing>(LISTINGS_COLLECTION_NAME);
-    const listings = await collection.find({}).limit(10).toArray();
+    const collection = db.collection(LISTINGS_COLLECTION_NAME);
+    const listings = await collection.aggregate<Listing>(pipeline).toArray();
+
     res.status(200).json(listings);
   } catch (err) {
     res.status(500).json([]);
--- typings/listings/Listing.d.ts
+++ typings/listings/Listing.d.ts
@@ -1,6 +1,10 @@
 type Listing = {
   _id: string;
-  listing_url: string;
   name: string;
   summary: string;
+  price: number;
+  images: Record<
+    'thumbnail_url' | 'medium_url' | 'picture_url' | 'xl_picture_url',
+    string
+  >;
 };
