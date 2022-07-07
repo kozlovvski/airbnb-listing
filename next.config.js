01192f297f1aae7a90dcc9609dbc4b68d4fa0eba
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-07 22:14:16 +0200

    fix: remove api-related constands. use absolute url

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  publicRuntimeConfig: {
-    PUBLIC_URL: process.env.VERCEL_URL || 'localhost:3000',
-  },
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV !== 'production',
--- services/listings/listings.api.service.ts
+++ services/listings/listings.api.service.ts
@@ -1,10 +1,9 @@
 import { QueryFunction } from 'react-query';
-import { NEXT_API_URL } from 'services/services.constants';
 
 export const getListingsFromAPIRoute: QueryFunction<Listing[]> = async ({
   signal,
 }) => {
-  const res = await fetch(`${NEXT_API_URL}/getListings`, {
+  const res = await fetch('/api/getListings', {
     signal,
   });
   return res.json();
