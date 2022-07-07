a8a72ffff5d38307b2277f4d7f669ecd4948a896
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-08 00:32:20 +0200

    feat: add basic styling to ListingListItem

--- components/listings/ListingList/ListingList.styles.ts
+++ components/listings/ListingList/ListingList.styles.ts
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
+import { WRAPPER_WIDTH } from 'styles/styles.contants';
 
-export const ListingListWrapper = styled.ul``;
-
-export const ListingListElement = styled.li<{ isSelected: boolean }>`
-  color: ${({ isSelected }) => (isSelected ? 'red' : 'blue')};
-  transform: scale(${({ isSelected }) => (isSelected ? 1.5 : 1)});
-  transition: color 1s ease-in-out, transform 1s ease-in-out;
+export const ListingListWrapper = styled.ul`
+  display: grid;
+  max-width: ${WRAPPER_WIDTH}px;
+  margin: 0 auto;
+  gap: 50px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 `;
