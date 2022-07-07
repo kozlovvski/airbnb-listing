1b7673384e997c58ac864ebee809282eeb89199e
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-08 01:57:38 +0200

    fix: adjust mongo service to prevent unnecessary connections

--- services/mongodb/mongodb.service.ts
+++ services/mongodb/mongodb.service.ts
@@ -1,29 +1,31 @@
-import { Db, MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+const options = {};
 
-type Connection = {
-  client: MongoClient;
-  db: Db;
-};
-
-let cachedConnection: Connection;
-
-export async function connectToDatabase() {
-  if (!mongoConnectionString) {
-    throw new Error('MONGO_CONNECTION_STRING env is not set');
-  }
-
-  if (cachedConnection) return cachedConnection;
-
-  const client = await MongoClient.connect(mongoConnectionString);
-  const db = client.db();
-
-  const connection = {
-    client,
-    db,
-  };
-
-  cachedConnection = connection;
-  return connection;
+if (!mongoConnectionString) {
+  throw new Error('MONGO_CONNECTION_STRING env is not set');
 }
+
+declare module global {
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(mongoConnectionString, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(mongoConnectionString, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
