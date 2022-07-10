18ed429633383b08b0dce4e745980b95346719fb
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-10 23:39:37 +0200

    feat: improve listing card design

--- components/listings/ListingList/ListingList.styles.ts
+++ components/listings/ListingList/ListingList.styles.ts
@@ -4,6 +4,7 @@ import { WRAPPER_WIDTH } from 'styles/styles.contants';
 export const ListingListWrapper = styled.ul`
   display: grid;
   max-width: ${WRAPPER_WIDTH}px;
+  padding: 0 20px;
   margin: 0 auto;
   gap: ${({ theme }) => theme.space[6]};
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
--- components/listings/ListingList/ListingListItem/ListingListItem.component.tsx
+++ components/listings/ListingList/ListingListItem/ListingListItem.component.tsx
@@ -1,13 +1,16 @@
 import { Text } from '@chakra-ui/react';
+import { MotionConfig, useMotionValue } from 'framer-motion';
 import { formatPrice } from 'helpers/price.helpers';
 import Link from 'next/link';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { DefinedListing } from 'typings/listings/Listing';
 
 import { getListingThumbnail } from './ListingListItem.helpers';
 import {
+  ListingListItemContent,
+  ListingListItemContentWrapper,
+  ListingListItemTextContainer,
   ListingListItemThumbnail,
-  ListingListItemThumbnailWrapper,
   ListingListItemWrapper,
 } from './ListingListItem.styles';
 import { ListingListItemOverlay } from './ListingListItemOverlay/ListingListItemOverlay.component';
@@ -17,19 +20,78 @@ type Props = {
   isSelected: boolean;
 };
 
+const ANIMATION_DURATION = 500;
+
 const ListingListItem = ({ listing, isSelected }: Props) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const y = useMotionValue(0);
+  const borderWidth = useMotionValue(isSelected ? 0 : 1);
+  const zIndex = useMotionValue(isSelected ? 2 : 0);
   const thumbnail = useMemo(() => getListingThumbnail(listing), [listing]);
   const { name, price } = listing;
 
+  useEffect(() => {
+    if (!isSelected) {
+      setTimeout(() => {
+        zIndex.set(0);
+        borderWidth.set(1);
+      }, ANIMATION_DURATION);
+    } else {
+      zIndex.set(1);
+      borderWidth.set(0);
+    }
+  }, [isSelected]);
+
   return (
-    <Link href={`/${listing._id}`}>
+    <Link href={`/${listing._id}`} scroll={false}>
       <ListingListItemWrapper data-testid="listing-list-item">
         <ListingListItemOverlay isSelected={isSelected} />
-        <ListingListItemThumbnailWrapper>
-          <ListingListItemThumbnail src={thumbnail} />
-        </ListingListItemThumbnailWrapper>
-        <Text fontWeight="bold">{name}</Text>
-        <Text color="gray.500">{formatPrice(price)} / night</Text>
+        <MotionConfig
+          transition={{
+            type: 'spring',
+            stiffness: 200,
+            damping: 30,
+            duration: ANIMATION_DURATION / 1000,
+          }}
+        >
+          <ListingListItemContentWrapper
+            isSelected={isSelected}
+            style={{ zIndex }}
+          >
+            <ListingListItemContent
+              ref={contentRef}
+              style={{ y, borderWidth }}
+              layout
+            >
+              <ListingListItemThumbnail
+                style={{
+                  originX: 'center',
+                  originY: 'center',
+                }}
+                src={thumbnail}
+                layout
+                isSelected={isSelected}
+              />
+              <ListingListItemTextContainer layout="position">
+                <Text
+                  fontWeight="bold"
+                  textOverflow="ellipsis"
+                  overflow="hidden"
+                  whiteSpace="nowrap"
+                >
+                  {name}
+                </Text>
+                <Text fontSize="sm">
+                  {formatPrice(price)}
+                  <Text as="span" color="gray.500">
+                    {' '}
+                    / night
+                  </Text>
+                </Text>
+              </ListingListItemTextContainer>
+            </ListingListItemContent>
+          </ListingListItemContentWrapper>
+        </MotionConfig>
       </ListingListItemWrapper>
     </Link>
   );
