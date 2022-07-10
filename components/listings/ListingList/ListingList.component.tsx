bcc34fc8e075b843cf0fc329e4b341493caec8eb
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-10 15:45:49 +0200

    feat: setup infinite scroll for listing list

--- components/listings/ListingList/ListingList.component.tsx
+++ components/listings/ListingList/ListingList.component.tsx
@@ -1,5 +1,6 @@
+import { useInView } from 'framer-motion';
 import { GetListingsQuery } from 'generated/graphql-codegen';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { ListingListWrapper } from './ListingList.styles';
 import ListingListItem from './ListingListItem/ListingListItem.component';
@@ -7,29 +8,42 @@ import ListingListItemSkeleton from './ListingListItem/ListingListItemSkeleton/L
 
 type Props = {
   pages: GetListingsQuery[] | undefined;
+  onSkeletonInView: () => void;
   selectedId?: string;
 };
 
-const ListingList = ({ pages, selectedId }: Props) => (
-  <ListingListWrapper>
-    {pages &&
-      pages.map(({ listings }) =>
-        listings
-          ? listings.map((listing) =>
-              listing ? (
-                <ListingListItem
-                  listing={listing}
-                  key={listing._id}
-                  isSelected={listing._id === selectedId}
-                />
-              ) : null
-            )
-          : null
-      )}
-    {[...Array(20)].map((_, i) => (
-      <ListingListItemSkeleton key={`skeleton_${i}`} />
-    ))}
-  </ListingListWrapper>
-);
+const ListingList = ({ pages, onSkeletonInView, selectedId }: Props) => {
+  const firstSkeletonRef = useRef<HTMLDivElement>(null);
+  const isSkeletonInView = useInView(firstSkeletonRef);
+
+  useEffect(() => {
+    isSkeletonInView && onSkeletonInView();
+  }, [isSkeletonInView, onSkeletonInView]);
+
+  return (
+    <ListingListWrapper>
+      {pages &&
+        pages?.map(({ listings }) =>
+          listings
+            ? listings.map((listing) =>
+                listing ? (
+                  <ListingListItem
+                    listing={listing}
+                    key={listing._id}
+                    isSelected={listing._id === selectedId}
+                  />
+                ) : null
+              )
+            : null
+        )}
+      {[...Array(20)].map((_, i) => (
+        <ListingListItemSkeleton
+          ref={i === 0 ? firstSkeletonRef : null}
+          key={`skeleton_${i}`}
+        />
+      ))}
+    </ListingListWrapper>
+  );
+};
 
 export default ListingList;
--- components/listings/ListingList/ListingListItem/ListingListItemSkeleton/ListingListItemSkeleton.component.tsx
+++ components/listings/ListingList/ListingListItem/ListingListItemSkeleton/ListingListItemSkeleton.component.tsx
@@ -1,18 +1,20 @@
 import { Skeleton, SkeletonText } from '@chakra-ui/react';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import {
   ListingListItemThumbnailWrapper,
   ListingListItemWrapper,
 } from '../ListingListItem.styles';
 
-const ListingListItemSkeleton = () => (
+const ListingListItemSkeleton = forwardRef<HTMLDivElement>((props, ref) => (
   <ListingListItemWrapper>
-    <ListingListItemThumbnailWrapper>
+    <ListingListItemThumbnailWrapper ref={ref} {...props}>
       <Skeleton pos="absolute" w="100%" h="100%" />
     </ListingListItemThumbnailWrapper>
     <SkeletonText mt={3} skeletonHeight={4} lineHeight={2} noOfLines={2} />
   </ListingListItemWrapper>
-);
+));
+
+ListingListItemSkeleton.displayName = 'ListingListItemSkeleton';
 
 export default ListingListItemSkeleton;
