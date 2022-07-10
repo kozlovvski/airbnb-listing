b504c8c8554887797462ffb1d4d393d796196abc
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-10 15:45:49 +0200

    feat: setup infinite scroll for listing list

--- layouts/ListingListLayout.tsx
+++ layouts/ListingListLayout.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@chakra-ui/react';
 import ListingList from 'components/listings/ListingList/ListingList.component';
 import { useInfiniteGetListingsQuery } from 'components/listings/ListingList/ListingList.hooks';
 import { useRouter } from 'next/router';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback } from 'react';
 
 /**
  * This Layout component is used to render listings in order to prevent
@@ -20,6 +19,9 @@ export const ListingListLayout = ({ children }: PropsWithChildren) => {
     }
   );
   const { listing_id } = useRouter().query;
+  const onSkeletonInView = useCallback(() => {
+    fetchNextPage();
+  }, [fetchNextPage]);
 
   if (Array.isArray(listing_id)) {
     console.error('[ListingLayouy] listing_id is an array');
@@ -28,8 +30,11 @@ export const ListingListLayout = ({ children }: PropsWithChildren) => {
 
   return (
     <div>
-      <ListingList pages={data?.pages} selectedId={listing_id} />
-      <Button onClick={() => fetchNextPage()}>Load more results</Button>
+      <ListingList
+        onSkeletonInView={onSkeletonInView}
+        pages={data?.pages}
+        selectedId={listing_id}
+      />
       {children}
     </div>
   );
