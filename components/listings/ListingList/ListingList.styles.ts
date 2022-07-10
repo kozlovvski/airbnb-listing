35c0660c773b9968f231e816be81feb02eb5b851
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-11 00:55:22 +0200

    fix: increase min listing card width to improve responsiveness

--- components/listings/ListingList/ListingList.styles.ts
+++ components/listings/ListingList/ListingList.styles.ts
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 import { WRAPPER_WIDTH } from 'styles/styles.contants';
 
+const MIN_CARD_WIDTH = 280;
+
 export const ListingListWrapper = styled.ul`
   display: grid;
   max-width: ${WRAPPER_WIDTH}px;
   padding: 0 20px;
   margin: 0 auto;
   gap: ${({ theme }) => theme.space[6]};
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(${MIN_CARD_WIDTH}px, 1fr));
 `;
