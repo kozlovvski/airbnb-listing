474dde9b0fe4d4a1a8c153369021f221c7aa3a9c
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-11 00:30:38 +0200

    test: add listing list helpers tests

--- components/listings/ListingList/ListingListItem/ListingListItem.test.tsx
+++ components/listings/ListingList/ListingListItem/ListingListItem.test.tsx
@@ -1,8 +1,11 @@
+import { theme } from '@chakra-ui/react';
 import { render } from '@testing-library/react';
 import { mockedListing } from 'tests/data/listings';
 import { wrapper } from 'tests/testUtils';
+import { DefinedListing } from 'typings/listings/Listing';
 
 import ListingListItem from './ListingListItem.component';
+import { getListingThumbnail, stringToColor } from './ListingListItem.helpers';
 
 describe('ListingListItem', () => {
   it('should render a list item', () => {
@@ -12,7 +15,6 @@ describe('ListingListItem', () => {
     const result = render(<ListingListItem listing={listing} isSelected />, {
       wrapper,
     });
-
     // then
     expect(result.getByRole('listitem')).toBeInTheDocument();
   });
@@ -24,7 +26,6 @@ describe('ListingListItem', () => {
     const result = render(<ListingListItem listing={listing} isSelected />, {
       wrapper,
     });
-
     // then
     expect(result.getByText('Listing 1')).toBeInTheDocument();
   });
@@ -36,7 +37,6 @@ describe('ListingListItem', () => {
     const result = render(<ListingListItem listing={listing} isSelected />, {
       wrapper,
     });
-
     // then
     expect(result.getByText(/\$50 USD/i)).toBeInTheDocument();
   });
@@ -48,8 +48,57 @@ describe('ListingListItem', () => {
     const result = render(<ListingListItem listing={listing} isSelected />, {
       wrapper,
     });
-
     // then
     expect(result.getByRole('img')).toBeInTheDocument();
+  });
+});
+
+describe('getListingThumbnail', () => {
+  it('should return an empty string if images are undefined', () => {
+    // given
+    const listing = {};
+    // when
+    const result = getListingThumbnail(listing);
+    // then
+    expect(result).toBe('');
+  });
+
+  it('should return an empty string if none url is provided', () => {
+    // given
+    const listing = { images: {} };
+    // when
+    const result = getListingThumbnail(listing);
+    // then
+    expect(result).toBe('');
+  });
+
+  it('should return a correct string', () => {
+    // given
+    const medium_url = 'test';
+    const listing: DefinedListing = { images: { medium_url } };
+    // when
+    const result = getListingThumbnail(listing);
+    // then
+    expect(result).toBe(medium_url);
+  });
+});
+
+describe('stringToColor', () => {
+  it('should return a gray color string if provided string is falsy', () => {
+    // given
+    const str = '';
+    // when
+    const result = stringToColor(str);
+    // then
+    expect(result).toBe(theme.colors.gray[200]);
+  });
+
+  it('should return a hex color string for a correct string', () => {
+    // given
+    const str = 'dummy string';
+    // when
+    const result = stringToColor(str);
+    // then
+    expect(result).toMatch(/^#[0-9A-F]{6}$/i);
   });
 });
