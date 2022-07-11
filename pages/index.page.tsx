51cc9a4ac97767f49585817658e878854f24047d
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-11 22:49:46 +0200

    fix: adjust getStaticProps in index page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,9 +1,4 @@
-import { useInfiniteGetListingsQuery } from 'components/listings/ListingList/ListingList.hooks';
-import { useGetListingsQuery } from 'generated/graphql-codegen';
 import { ListingListLayout } from 'layouts/ListingListLayout';
-import type { GetStaticProps } from 'next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { dehydrate, QueryClient } from 'react-query';
 import { NextPageWithLayout } from 'typings/NextPageWithLayout';
 
 const Home: NextPageWithLayout = () => {
@@ -12,18 +7,40 @@ const Home: NextPageWithLayout = () => {
 
 Home.getLayout = (page) => <ListingListLayout>{page}</ListingListLayout>;
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(useInfiniteGetListingsQuery.getKey(), () =>
-    useGetListingsQuery.fetcher()()
-  );
+// export const getStaticProps: GetStaticProps = async ({ locale }) => {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery(
+//     useInfiniteGetListingsQuery.getKey(),
+//     async () => {
+//       if (!process.env.NEXT_APP_MONGO_GRAPHQL_ENDPOINT) {
+//         throw Error('NEXT_APP_MONGO_GRAPHQL_ENDPOINT is not defined');
+//       }
 
-  return {
-    props: {
-      ...(await serverSideTranslations(locale!, ['common', 'listings'])),
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
+//       const res = await fetch(process.env.NEXT_APP_MONGO_GRAPHQL_ENDPOINT, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//           query: GetListingsDocument,
+//           variables: {
+//             pageParam: 0,
+//           },
+//         }),
+//         headers: {
+//           apiKey: process.env.NEXT_APP_MONGO_ATLAS_API_KEY || '',
+//         },
+//       });
+
+//       const json = await res.json();
+
+//       return json.data;
+//     }
+//   );
+
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale!, ['common', 'listings'])),
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
 
 export default Home;
