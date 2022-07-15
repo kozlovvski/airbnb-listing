89aa9c9af3692fb0c66a191f1868d3c402d23477
Michał Kozłowski <michal.kozlowski@start-up.house>
2022-07-16 00:08:08 +0200

    feat: WIP new listing route

--- components/listings/ListingList/ListingListItem/ListingListItem.component.tsx
+++ components/listings/ListingList/ListingListItem/ListingListItem.component.tsx
@@ -1,8 +1,7 @@
 import { Badge, Flex, Text } from '@chakra-ui/react';
-import { MotionConfig, useMotionValue } from 'framer-motion';
 import { formatPrice } from 'helpers/price.helpers';
 import Link from 'next/link';
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import { DefinedListing } from 'typings/listings/Listing';
 
 import { getListingThumbnail, stringToColor } from './ListingListItem.helpers';
@@ -13,40 +12,22 @@ import {
   ListingListItemThumbnail,
   ListingListItemWrapper,
 } from './ListingListItem.styles';
-import { ListingListItemOverlay } from './ListingListItemOverlay/ListingListItemOverlay.component';
 
 type Props = {
   listing: DefinedListing;
-  isSelected: boolean;
 };
 
-const ANIMATION_DURATION = 500;
 const separator = (
   <Text fontSize="xs" mx={1}>
     •
   </Text>
 );
 
-const ListingListItem = ({ listing, isSelected }: Props) => {
+const ListingListItem = ({ listing }: Props) => {
   const contentRef = useRef<HTMLDivElement>(null);
-  const y = useMotionValue(0);
-  const borderWidth = useMotionValue(isSelected ? 0 : 1);
-  const zIndex = useMotionValue(isSelected ? 2 : 0);
   const thumbnail = useMemo(() => getListingThumbnail(listing), [listing]);
   const { name, price, property_type, beds } = listing;
   const bathrooms = listing.bathrooms ? Number(listing.bathrooms) : undefined;
-
-  useEffect(() => {
-    if (!isSelected) {
-      setTimeout(() => {
-        zIndex.set(0);
-        borderWidth.set(1);
-      }, ANIMATION_DURATION);
-    } else {
-      zIndex.set(1);
-      borderWidth.set(0);
-    }
-  }, [isSelected]);
 
   const bedsContent = beds && (
     <>
@@ -67,62 +48,37 @@ const ListingListItem = ({ listing, isSelected }: Props) => {
   );
 
   return (
-    <Link href={`/${listing._id}`} scroll={false}>
+    <Link href={`/${listing._id}`} prefetch={false}>
       <ListingListItemWrapper data-testid="listing-list-item">
-        <ListingListItemOverlay isSelected={isSelected} />
-        <MotionConfig
-          transition={{
-            type: 'spring',
-            stiffness: 200,
-            damping: 30,
-            duration: ANIMATION_DURATION / 1000,
-          }}
-        >
-          <ListingListItemContentWrapper
-            isSelected={isSelected}
-            style={{ zIndex }}
-          >
-            <ListingListItemContent
-              ref={contentRef}
-              style={{ y, borderWidth }}
-              layout
-            >
-              <ListingListItemThumbnail
-                style={{
-                  originX: 'center',
-                  originY: 'center',
-                }}
-                src={thumbnail}
-                layout
-                isSelected={isSelected}
-              />
-              <ListingListItemTextContainer layout="position">
-                <Flex mb={1}>
-                  <Badge color={stringToColor(property_type)}>
-                    {property_type}
-                  </Badge>
-                  {bedsContent}
-                  {bathroomsContent}
-                </Flex>
-                <Text
-                  fontWeight="bold"
-                  textOverflow="ellipsis"
-                  overflow="hidden"
-                  whiteSpace="nowrap"
-                >
-                  {name}
+        <ListingListItemContentWrapper>
+          <ListingListItemContent ref={contentRef}>
+            <ListingListItemThumbnail src={thumbnail} />
+            <ListingListItemTextContainer>
+              <Flex mb={1}>
+                <Badge color={stringToColor(property_type)}>
+                  {property_type}
+                </Badge>
+                {bedsContent}
+                {bathroomsContent}
+              </Flex>
+              <Text
+                fontWeight="bold"
+                textOverflow="ellipsis"
+                overflow="hidden"
+                whiteSpace="nowrap"
+              >
+                {name}
+              </Text>
+              <Text fontSize="sm">
+                {formatPrice(price)}
+                <Text as="span" color="gray.500">
+                  {' '}
+                  / night
                 </Text>
-                <Text fontSize="sm">
-                  {formatPrice(price)}
-                  <Text as="span" color="gray.500">
-                    {' '}
-                    / night
-                  </Text>
-                </Text>
-              </ListingListItemTextContainer>
-            </ListingListItemContent>
-          </ListingListItemContentWrapper>
-        </MotionConfig>
+              </Text>
+            </ListingListItemTextContainer>
+          </ListingListItemContent>
+        </ListingListItemContentWrapper>
       </ListingListItemWrapper>
     </Link>
   );
